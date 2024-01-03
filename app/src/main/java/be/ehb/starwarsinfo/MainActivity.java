09d3900901172b25673c11ec380b018e60452f5d
Mat. Devski Carlier <carliermateo@outlook.be>
2024-01-03 09:39:38 +0100

    Made a request to the API, and displayed its response's content in the activity_main view for testing purposes

--- app/src/main/java/be/ehb/starwarsinfo/MainActivity.java
+++ app/src/main/java/be/ehb/starwarsinfo/MainActivity.java
@@ -3,12 +3,45 @@ package be.ehb.starwarsinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvResult = findViewById(R.id.tv_result);
+
+        Thread backgroundThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient mClient = new OkHttpClient();
+                    Request mRequest = new Request.Builder()
+                            .url("https://swapi.dev/api/planets/1/")
+                            .get()
+                            .build();
+                    Response mResponse = mClient.newCall(mRequest).execute();
+                    String responseText = mResponse.body().string();
+
+                    tvResult.setText(responseText);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        backgroundThread.start();
     }
 }
