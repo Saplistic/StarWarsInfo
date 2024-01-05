737189fea3f2cebd787d45e982d14de6bdc8555f
Mat. Devski Carlier <carliermateo@outlook.be>
2024-01-05 18:21:22 +0100

    Implemented the Room dependency for storing data locally

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
