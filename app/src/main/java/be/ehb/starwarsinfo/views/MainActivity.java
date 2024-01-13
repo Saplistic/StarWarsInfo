e9d7211c81bd47ee1ef31a10733935b5ebd1e4ac
Mat. Devski Carlier <carliermateo@outlook.be>
2024-01-13 16:52:25 +0100

    Moved api call thread outside onCreate method & tweaked it

--- app/src/main/java/be/ehb/starwarsinfo/views/MainActivity.java
+++ app/src/main/java/be/ehb/starwarsinfo/views/MainActivity.java
@@ -15,7 +15,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import be.ehb.starwarsinfo.R;
 import be.ehb.starwarsinfo.model.Planet;
 import be.ehb.starwarsinfo.model.SWInfoDatabase;
@@ -33,73 +32,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread backgroundThread = new Thread(() ->
-        {
-            try {
-                SWInfoDB = SWInfoDatabase.getInstance(getApplicationContext());
-                SWInfoDB.getPlanetDAO().nukePlanetsTable();
-
-                OkHttpClient mClient = new OkHttpClient();
-
-                int currentPage = 1;
-                boolean lastPageReached = false;
-
-                while (!lastPageReached) {
-                    Request mRequest = new Request.Builder()
-                        .url(API_URL + "/?page=" + currentPage)
-                        .get()
-                        .build();
-                    Response mResponse = mClient.newCall(mRequest).execute();
-
-                    String responseText = mResponse.body().string();
-                    JSONObject ResultJSONObject = new JSONObject(responseText);
-                    JSONArray planetsJSON = new JSONArray(ResultJSONObject.getString("results"));
-
-                    //Log.d("LOG", responseText);
-
-                    if (ResultJSONObject.isNull("next")) {
-                        lastPageReached = true;
-                    }
-
-                    int length = planetsJSON.length();
-                    ArrayList<Planet> retrievedPlanets = new ArrayList<>(length);
-
-                    for (int i = 0; i < length; i++) {
-                        JSONObject tempPlanet = planetsJSON.getJSONObject(i);
-
-                        Planet planet = new Planet(
-                            tempPlanet.getString("name"),
-                            tempPlanet.getString("rotation_period"),
-                            tempPlanet.getString("orbital_period"),
-                            tempPlanet.getString("diameter"),
-                            tempPlanet.getString("climate"),
-                            tempPlanet.getString("gravity"),
-                            tempPlanet.getString("terrain"),
-                            tempPlanet.getString("surface_water"),
-                            tempPlanet.getString("population")
-                        );
-                        retrievedPlanets.add(planet);
-                    }
-                    SWInfoDB.getPlanetDAO().insertAll(retrievedPlanets);
-
-                    currentPage++;
-                }
-                runOnUiThread(() -> Toast.makeText(getApplicationContext(),"Succesfully updated data", Toast.LENGTH_LONG).show());
-
-            } catch (IOException e) {
-                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Network error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
-            } catch (JSONException e) {
-                runOnUiThread(() -> Toast.makeText(MainActivity.this, "JSON parsing error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
-            }
-        });
-
         if (isNetworkAvailable()){
             backgroundThread.start();
         } else {
-            //Log.d("TAG", "No internet connection found");
             Toast.makeText(getApplicationContext(),"No data was updated, no internet connection found", Toast.LENGTH_LONG).show();
         }
     }
+
+    Thread backgroundThread = new Thread(() ->
+    {
+        try {
+            SWInfoDB = SWInfoDatabase.getInstance(getApplicationContext());
+            SWInfoDB.getPlanetDAO().nukePlanetsTable();
+
+            OkHttpClient mClient = new OkHttpClient();
+
+            int currentPage = 1;
+            boolean lastPageReached = false;
+
+            while (!lastPageReached) {
+                Request mRequest = new Request.Builder()
+                        .url(API_URL + "/?page=" + currentPage)
+                        .get()
+                        .build();
+                Response mResponse = mClient.newCall(mRequest).execute();
+
+                String responseText = mResponse.body().string();
+                JSONObject ResultJSONObject = new JSONObject(responseText);
+                JSONArray planetsJSON = new JSONArray(ResultJSONObject.getString("results"));
+
+                if (ResultJSONObject.isNull("next")) {
+                    lastPageReached = true;
+                }
+
+                int length = planetsJSON.length();
+                ArrayList<Planet> retrievedPlanets = new ArrayList<>(length);
+
+                for (int i = 0; i < length; i++) {
+                    JSONObject tempPlanet = planetsJSON.getJSONObject(i);
+
+                    Planet planet = new Planet(
+                        tempPlanet.getString("name"),
+                        tempPlanet.getString("rotation_period"),
+                        tempPlanet.getString("orbital_period"),
+                        tempPlanet.getString("diameter"),
+                        tempPlanet.getString("climate"),
+                        tempPlanet.getString("gravity"),
+                        tempPlanet.getString("terrain"),
+                        tempPlanet.getString("surface_water"),
+                        tempPlanet.getString("population")
+                    );
+                    retrievedPlanets.add(planet);
+                }
+                SWInfoDB.getPlanetDAO().insertAll(retrievedPlanets);
+
+                currentPage++;
+            }
+
+            runOnUiThread(() -> Toast.makeText(getApplicationContext(),"Succesfully updated data", Toast.LENGTH_LONG).show());
+
+        } catch (IOException e) {
+            runOnUiThread(() -> Toast.makeText(MainActivity.this, "Network error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+        } catch (JSONException e) {
+            runOnUiThread(() -> Toast.makeText(MainActivity.this, "JSON parsing error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+        }
+    });
 
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
