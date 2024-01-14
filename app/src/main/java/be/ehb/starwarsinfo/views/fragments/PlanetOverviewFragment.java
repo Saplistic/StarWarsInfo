44f5ecb17d1869ccf4af54906bc128cc147379d0
Mat. Devski Carlier <carliermateo@outlook.be>
2024-01-14 13:45:01 +0100

    Created a search function for the planets

--- app/src/main/java/be/ehb/starwarsinfo/views/fragments/PlanetOverviewFragment.java
+++ app/src/main/java/be/ehb/starwarsinfo/views/fragments/PlanetOverviewFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,7 +26,9 @@ import be.ehb.starwarsinfo.views.fragments.util.PlanetAdapter;
 public class PlanetOverviewFragment extends Fragment {
 
     private SWInfoDatabase SWInfoDB;
-    RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerView;
+    private PlanetAdapter mAdapter = new PlanetAdapter();
+    private ArrayList<Planet> items;
 
     public PlanetOverviewFragment() {
     }
@@ -30,6 +36,7 @@ public class PlanetOverviewFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setHasOptionsMenu(true);
     }
 
     @Override
@@ -45,13 +52,48 @@ public class PlanetOverviewFragment extends Fragment {
         SWInfoDB = SWInfoDatabase.getInstance(getContext());
 
         mRecyclerView = view.findViewById(R.id.rv_planets_overview);
-        PlanetAdapter mAdapter = new PlanetAdapter();
+        mAdapter = new PlanetAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
 
         SWInfoDB.getPlanetDAO().getAllPlanets().observe(getViewLifecycleOwner(), planets -> {
-            mAdapter.setItems((ArrayList<Planet>) planets);
-
+            items = (ArrayList<Planet>) planets;
+            mAdapter.setItems(items);
             mRecyclerView.setAdapter(mAdapter);
-            mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.overview_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.setItems(filterList(items, newText));
+                mRecyclerView.setAdapter(mAdapter);
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private ArrayList<Planet> filterList(ArrayList<Planet> list, String filterQuery) {
+        ArrayList<Planet> filteredList = new ArrayList<>();
+
+        for (Planet planet : list) {
+            if (planet.getName().toLowerCase().contains(filterQuery.toLowerCase())) {
+                filteredList.add(planet);
+            }
+        }
+        return filteredList;
     }
 }
